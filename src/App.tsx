4a5e2d0bfc4193b816304854acc3643211e8c4f0
ILzat <ilzat.sadykov.2006@bk.ru>
2024-06-17 15:28:05 +0600

    "Добавил POST запрос"

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import axios, {AxiosError} from "axios";
 import {useEffect, useState} from "react";
-import {Message,} from "../types.ts";
+import {Message, newMessage} from "../types.ts";
 import Messages from "./Components/Messages/Messages.tsx";
 import SendMessage from "./Components/Messages/SendMessage.tsx";
 
@@ -10,19 +10,32 @@ const App = () => {
     const [posts, setPosts] = useState<Message[]>([]);
     const [error, setError] = useState<Error | null>(null);
     const url = 'http://146.185.154.90:8000/messages';
+
+    const fetchData = async () => {
+        try {
+            const result = await axios.get(url)
+            setPosts(result.data);
+        } catch (error) {
+            setError(error as AxiosError);
+        }
+    };
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const result = await axios.get(url)
-                setPosts(result.data);
-            } catch (error) {
-                setError(error as AxiosError);
-            }
-        };
         void fetchData();
         const threeSecond = setInterval(fetchData, 3000);
         return () => clearInterval(threeSecond);
     }, []);
+
+    const sendNewMessage = async (newMessage: newMessage) => {
+        try {
+            const data = new URLSearchParams();
+            data.set('author', newMessage.author);
+            data.set('message', newMessage.message);
+            await axios.post(url, data);
+            void fetchData()
+        } catch (error) {
+            setError(error as AxiosError);
+        }
+    }
 
     if (error) {
         return <div>{error.message}</div>;
@@ -35,7 +48,7 @@ const App = () => {
                     ))}
                 </div>
                 <div>
-                    <SendMessage/>
+                    <SendMessage onSendMessage={sendNewMessage}/>
                 </div>
             </>
         )
--- src/Components/Messages/SendMessage.tsx
+++ src/Components/Messages/SendMessage.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {FormEvent, useState} from 'react';
+import {newMessage} from "../../../types.ts";
 
-const SendMessage = () => {
+interface Props {
+    onSendMessage: (message: newMessage) => void;
+}
+
+const SendMessage: React.FC<Props> = ({onSendMessage}) => {
+    const [author, setAuthor] = useState('');
+    const [message, setMessage] = useState('');
+    const submitMessage = (e: FormEvent) => {
+        e.preventDefault();
+        onSendMessage({author, message});
+        setMessage('')
+    }
     return (
-        <form>
-            <input type={"text"} placeholder={"Author"}/>
-            <input type={"text"} placeholder={"Message"}/>
+        <form onSubmit={submitMessage}>
+            <input
+                type={"text"}
+                value={author}
+                placeholder={"Author"}
+                onChange={(e) => setAuthor(e.target.value)}
+            />
+            <input
+                type={"text"}
+                value={message}
+                placeholder={"Message"}
+                onChange={(e) => setMessage(e.target.value)}
+            />
             <button type={"submit"}>Send</button>
         </form>
     );
